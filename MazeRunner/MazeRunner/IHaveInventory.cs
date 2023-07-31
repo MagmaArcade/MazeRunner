aca43418c2d35d751b572e5d1053e3cef1477bee
MagmaArcade <102445046+MagmaArcade@users.noreply.github.com>
2023-07-31 21:19:39 +1000

    fixes

---
+++ MazeRunner/MazeRunner/IHaveInventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze_Runner
+{
+    public interface IHaveInventory
+    {
+        GameObject Locate(string id);
+        GameObject Take(string id);
+        void Put(Item item);
+        string Name { get; }
+    }
+}
---
+++ MazeRunner/NUnitTesting/MoveTest.cs
@@ -0,0 +1,9 @@
+﻿/*using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Swin_Adventure;
+
+namespace Identifiable_Object_Unit_Tests
+{
+  
+}
+*/
